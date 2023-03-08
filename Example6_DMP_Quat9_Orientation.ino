73260a333f513aee73f7c172b28ed8bfc4cf733f
David Bechert <dbechert@sfu.ca>
2023-03-08 13:23:03 -0800

    added code for 2 sensors

--- Example6_DMP_Quat9_Orientation.ino
+++ Example6_DMP_Quat9_Orientation.ino
@@ -32,6 +32,8 @@
 
 #define SERIAL_PORT Serial
 
+#define USE_2_SENSORS
+
 #define SPI_PORT SPI // Your desired SPI port.       Used only when "USE_SPI" is defined
 #define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
 
@@ -39,14 +41,16 @@
 // The value of the last bit of the I2C address.
 // On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
 #define AD0_VAL 1
+#define AD0_VAL_1 0
+
 
 #ifdef USE_SPI
 ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
 #else
-ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
+ICM_20948_I2C myICM[2]; // Otherwise create an ICM_20948_I2C object
 #endif
 void quat_to_euler(float* angles, double q0, double q1, double q2, double q3);
-
+void process_data(icm_20948_DMP_data_t * data, ICM_20948_I2C* myICM);
 void setup()
 {
 
@@ -75,7 +79,8 @@ void setup()
 #endif
 
 #ifndef QUAT_ANIMATION
-  //myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+  myICM[0].enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+  myICM[1].enableDebugging();
 #endif
 
   bool initialized = false;
@@ -87,14 +92,22 @@ void setup()
 #ifdef USE_SPI
     myICM.begin(CS_PIN, SPI_PORT);
 #else
-    myICM.begin(WIRE_PORT, AD0_VAL);
+    myICM[0].begin(WIRE_PORT, AD0_VAL); // WAS AD0_VAL
+    #ifdef USE_2_SENSORS
+    myICM[1].begin(WIRE_PORT, AD0_VAL_1);
+    #endif
 #endif
 
 #ifndef QUAT_ANIMATION
     SERIAL_PORT.print(F("Initialization of the sensor returned: "));
-    SERIAL_PORT.println(myICM.statusString());
+    SERIAL_PORT.println(myICM[0].statusString());
+    SERIAL_PORT.println(myICM[1].statusString());
 #endif
-    if (myICM.status != ICM_20948_Stat_Ok)
+    #ifdef USE_2_SENSORS
+    if (myICM[0].status != ICM_20948_Stat_Ok || myICM[1].status != ICM_20948_Stat_Ok)
+    #else 
+    if (myICM[0].status != ICM_20948_Stat_Ok)
+    #endif
     {
 #ifndef QUAT_ANIMATION
       SERIAL_PORT.println(F("Trying again..."));
@@ -114,8 +127,11 @@ void setup()
   bool success = true; // Use success to show if the DMP configuration was successful
 
   // Initialize the DMP. initializeDMP is a weak function. You can overwrite it if you want to e.g. to change the sample rate
-  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
+  success &= (myICM[0].initializeDMP() == ICM_20948_Stat_Ok);
 
+  #ifdef USE_2_SENSORS
+  success &= (myICM[1].initializeDMP() == ICM_20948_Stat_Ok);
+  #endif
   // DMP sensor options are defined in ICM_20948_DMP.h
   //    INV_ICM20948_SENSOR_ACCELEROMETER               (16-bit accel)
   //    INV_ICM20948_SENSOR_GYROSCOPE                   (16-bit gyro + 32-bit calibrated gyro)
@@ -134,7 +150,11 @@ void setup()
   //    INV_ICM20948_SENSOR_ORIENTATION                 (32-bit 9-axis quaternion + heading accuracy)
 
   // Enable the DMP orientation sensor
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
+  success &= (myICM[0].enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
+  
+  #ifdef USE_2_SENSORS
+  success &= (myICM[1].enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
+  #endif
 
   // Enable any additional sensors / features
   //success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_GYROSCOPE) == ICM_20948_Stat_Ok);
@@ -146,7 +166,12 @@ void setup()
   // Setting value can be calculated as follows:
   // Value = (DMP running rate / ODR ) - 1
   // E.g. For a 5Hz ODR rate when DMP is running at 55Hz, value = (55/5) - 1 = 10.
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat9, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM[0].setDMPODRrate(DMP_ODR_Reg_Quat9, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+
+  #ifdef USE_2_SENSORS
+  success &= (myICM[1].setDMPODRrate(DMP_ODR_Reg_Quat9, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  #endif
+
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
@@ -154,16 +179,28 @@ void setup()
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
 
   // Enable the FIFO
-  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
+  success &= (myICM[0].enableFIFO() == ICM_20948_Stat_Ok);
+  #ifdef USE_2_SENSORS
+  success &= (myICM[1].enableFIFO() == ICM_20948_Stat_Ok);
+  #endif
 
   // Enable the DMP
-  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
+  success &= (myICM[0].enableDMP() == ICM_20948_Stat_Ok);
+    #ifdef USE_2_SENSORS
+  success &= (myICM[1].enableDMP() == ICM_20948_Stat_Ok);
+  #endif
 
   // Reset DMP
-  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
+  success &= (myICM[0].resetDMP() == ICM_20948_Stat_Ok);
+  #ifdef USE_2_SENSORS
+  success &= (myICM[1].resetDMP() == ICM_20948_Stat_Ok);
+  #endif 
 
   // Reset FIFO
-  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
+  success &= (myICM[0].resetFIFO() == ICM_20948_Stat_Ok);
+  #ifdef USE_2_SENSORS
+  success &= (myICM[1].resetFIFO() == ICM_20948_Stat_Ok);
+  #endif
 
   // Check success
   if (success)
@@ -190,82 +227,19 @@ void loop()
   //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOIncompleteData if a frame was present but was incomplete
   //    readDMPdataFromFIFO will return ICM_20948_Stat_Ok if a valid frame was read.
   //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOMoreDataAvail if a valid frame was read _and_ the FIFO contains more (unread) data.
-  icm_20948_DMP_data_t data;
-  myICM.readDMPdataFromFIFO(&data);
+  icm_20948_DMP_data_t data[2];
+  myICM[0].readDMPdataFromFIFO(&data[0]);
+  #ifdef USE_2_SENSORS
+  myICM[1].readDMPdataFromFIFO(&data[1]);
+  #endif
 
-  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
-  {
-    //SERIAL_PORT.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
-    //if ( data.header < 0x1000) SERIAL_PORT.print( "0" ); // Pad the zeros
-    //if ( data.header < 0x100) SERIAL_PORT.print( "0" );
-    //if ( data.header < 0x10) SERIAL_PORT.print( "0" );
-    //SERIAL_PORT.println( data.header, HEX );
+  process_data(&data[0], &myICM[0], 0);
 
-    if ((data.header & DMP_header_bitmap_Quat9) > 0) // We have asked for orientation data so we should receive Quat9
-    {
-      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
-      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
-      // The quaternion data is scaled by 2^30.
+  #ifdef USE_2_SENSORS
+  process_data(&data[1], &myICM[1], 1);
+  #endif
 
-      //SERIAL_PORT.printf("Quat9 data is: Q1:%ld Q2:%ld Q3:%ld Accuracy:%d\r\n", data.Quat9.Data.Q1, data.Quat9.Data.Q2, data.Quat9.Data.Q3, data.Quat9.Data.Accuracy);
-
-      // Scale to +/- 1
-      double q1 = ((double)data.Quat9.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q2 = ((double)data.Quat9.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q3 = ((double)data.Quat9.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
-      float angles[3];
-      quat_to_euler(angles, q0, q1, q2, q3);
-
-#ifndef QUAT_ANIMATION
-/*
-      SERIAL_PORT.print(F("Q0:"));
-      SERIAL_PORT.print(q0, 3);
-      SERIAL_PORT.print(F(" Q1:"));
-      SERIAL_PORT.print(q1, 3);
-      SERIAL_PORT.print(F(" Q2:"));
-      SERIAL_PORT.print(q2, 3);
-      SERIAL_PORT.print(F(" Q3:"));
-      SERIAL_PORT.print(q3, 3);
-      
-      SERIAL_PORT.print(F(" roll:"));
-      SERIAL_PORT.print(angles[0], 3);
-      SERIAL_PORT.print(F(" pitch:"));
-      SERIAL_PORT.print(angles[1], 3);
-      SERIAL_PORT.print(F(" yaw:"));
-      SERIAL_PORT.print(angles[2], 3);
-      SERIAL_PORT.print(F(" Accuracy:"));
-      SERIAL_PORT.println(data.Quat9.Data.Accuracy);
-
-      */
-
-      SERIAL_PORT.print(angles[0], 3);
-      SERIAL_PORT.print(F(","));
-      SERIAL_PORT.print(angles[1], 3);
-      SERIAL_PORT.print(F(","));
-      SERIAL_PORT.print(angles[2], 3);
-      SERIAL_PORT.print(F(","));
-      SERIAL_PORT.print(angles[1], 3);
-
-
-      SERIAL_PORT.write(10);
-      SERIAL_PORT.write(13);
-#else
-      // Output the Quaternion data in the format expected by ZaneL's Node.js Quaternion animation tool
-      SERIAL_PORT.print(F("{\"quat_w\":"));
-      SERIAL_PORT.print(q0, 3);
-      SERIAL_PORT.print(F(", \"quat_x\":"));
-      SERIAL_PORT.print(q1, 3);
-      SERIAL_PORT.print(F(", \"quat_y\":"));
-      SERIAL_PORT.print(q2, 3);
-      SERIAL_PORT.print(F(", \"quat_z\":"));
-      SERIAL_PORT.print(q3, 3);
-      SERIAL_PORT.println(F("}"));
-#endif
-    }
-  }
-
-  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
+  if (myICM[0].status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
   {
     delay(10);
   }
@@ -290,8 +264,89 @@ void quat_to_euler(float* angles, double q0, double q1, double q2, double q3){
     angles[0] *= 180.0f/3.14159265358979323846f;
     angles[1] *= 180.0f/3.14159265358979323846f;
     angles[2] *= 180.0f/3.14159265358979323846f;
-
-
 }
 
+void process_data(icm_20948_DMP_data_t * data, ICM_20948_I2C* myICM, int id){
+  if ((myICM->status == ICM_20948_Stat_Ok) || (myICM->status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
+  {
+    //SERIAL_PORT.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
+    //if ( data.header < 0x1000) SERIAL_PORT.print( "0" ); // Pad the zeros
+    //if ( data.header < 0x100) SERIAL_PORT.print( "0" );
+    //if ( data.header < 0x10) SERIAL_PORT.print( "0" );
+    //SERIAL_PORT.println( data.header, HEX );
+
+    if ((data->header & DMP_header_bitmap_Quat9) > 0) // We have asked for orientation data so we should receive Quat9
+    {
+      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
+      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
+      // The quaternion data is scaled by 2^30.
+
+      //SERIAL_PORT.printf("Quat9 data is: Q1:%ld Q2:%ld Q3:%ld Accuracy:%d\r\n", data.Quat9.Data.Q1, data.Quat9.Data.Q2, data.Quat9.Data.Q3, data.Quat9.Data.Accuracy);
+
+      // Scale to +/- 1
+      double q1 = ((double)data->Quat9.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q2 = ((double)data->Quat9.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q3 = ((double)data->Quat9.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
+      float angles[3];
+      quat_to_euler(angles, q0, q1, q2, q3);
+
+#ifndef QUAT_ANIMATION
+
+      /*SERIAL_PORT.print(F("Q0:"));
+      SERIAL_PORT.print(q0, 3);
+      SERIAL_PORT.print(F(" Q1:"));
+      SERIAL_PORT.print(q1, 3);
+      SERIAL_PORT.print(F(" Q2:"));
+      SERIAL_PORT.print(q2, 3);
+      SERIAL_PORT.print(F(" Q3:"));
+      SERIAL_PORT.print(q3, 3);*/
+      char* angle_names[3];
+      if(id == 0){
+        angle_names[0] = (" roll0:");
+        angle_names[1] = (" pitch0:");
+        angle_names[2] = (" yaw0:");
+      } else {
+        angle_names[0] = (" roll1:");
+        angle_names[1] = (" pitch1:");
+        angle_names[2] = (" yaw1:");
+      }
+
+      SERIAL_PORT.print(angle_names[0]);
+      SERIAL_PORT.print(angles[0], 3);
+      SERIAL_PORT.print(angle_names[1]);
+      SERIAL_PORT.print(angles[1], 3);
+      SERIAL_PORT.print(angle_names[2]);
+      SERIAL_PORT.println(angles[2], 3);
+      //SERIAL_PORT.print(F(" Accuracy:"));
+      //SERIAL_PORT.println(data.Quat9.Data.Accuracy);
+
+    
+    /*
+      SERIAL_PORT.print(angles[0], 3);
+      SERIAL_PORT.print(F(","));
+      SERIAL_PORT.print(angles[1], 3);
+      SERIAL_PORT.print(F(","));
+      SERIAL_PORT.print(angles[2], 3);
+      SERIAL_PORT.print(F(","));
+      SERIAL_PORT.print(angles[1], 3);
+
+      SERIAL_PORT.write(10);
+      SERIAL_PORT.write(13);
+      */
+#else
+      // Output the Quaternion data in the format expected by ZaneL's Node.js Quaternion animation tool
+      SERIAL_PORT.print(F("{\"quat_w\":"));
+      SERIAL_PORT.print(q0, 3);
+      SERIAL_PORT.print(F(", \"quat_x\":"));
+      SERIAL_PORT.print(q1, 3);
+      SERIAL_PORT.print(F(", \"quat_y\":"));
+      SERIAL_PORT.print(q2, 3);
+      SERIAL_PORT.print(F(", \"quat_z\":"));
+      SERIAL_PORT.print(q3, 3);
+      SERIAL_PORT.println(F("}"));
+#endif
+    }
+  }
+}
 
